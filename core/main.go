a59cb136a82318bee714d24c0a998af1db48cfeb
dmxmss <dexammt@gmail.com>
2025-05-28 19:07:58 +0300

    core app

---
+++ core/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+
+	"net/http"
+	"time"
+)
+
+func main() {
+	e := echo.New()
+
+	delaySeconds := 3	
+
+	e.GET(`/:text`, func(c echo.Context) error {
+			param := c.Param("text")
+
+			time.Sleep(time.Duration(delaySeconds) * time.Second)
+
+			return c.String(http.StatusOK, param)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
