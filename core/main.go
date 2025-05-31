9db14ba47e1fb5bd773cb65312ab8957355c3e06
dmxmss <dexammt@gmail.com>
2025-05-31 12:44:47 +0300

    templates

--- core/main.go
+++ core/main.go
@@ -2,22 +2,43 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
 	"net/http"
+	"html/template"
 	"time"
+	"io"
 )
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+
+	renderer := &Template{
+		templates: template.Must(template.ParseGlob("templates/*.html")),
+	}
+	e.Renderer = renderer
 
 	delaySeconds := 3	
 
 	e.GET(`/:text`, func(c echo.Context) error {
 			param := c.Param("text")
 
+			data := map[string]string{
+				"param": param,
+			}
+
 			time.Sleep(time.Duration(delaySeconds) * time.Second)
 
-			return c.String(http.StatusOK, param)
+			return c.Render(http.StatusOK, "index.html", data)
 	})
 
 	e.Logger.Fatal(e.Start(":3000"))
